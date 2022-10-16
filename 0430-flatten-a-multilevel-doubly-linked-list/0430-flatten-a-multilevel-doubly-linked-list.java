f2ebcb6d285bf8b08ef9433aa7c55510d14c3a6a
sragvisai <47244988+sragvisai@users.noreply.github.com>
2022-10-16 15:28:22 -0400

    Time: 6 ms (5.04%), Space: 42.5 MB (19.71%) - LeetHub

---
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,78 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    
+    List<Integer> list = new ArrayList<>();
+    
+    public Node flatten(Node head) {
+        
+        //a recursive dfs approach
+        
+        //when we encounter a child node call the function
+        //passing the current node as a parameter
+        
+        if(head == null) return head;
+        
+        
+        Node traverse = head;
+        
+        Node res = null;
+
+        
+        dfs(traverse,traverse.next);
+        
+        System.out.println("Here "+list);
+        
+        if(list.size() == 0) return res;
+        
+        res = new Node();
+        res.val = list.get(0);
+        res.prev = null;
+        
+        traverse = res;
+        
+        for(int i = 1 ;i < list.size() ; i++){
+            
+            Node node = new Node();
+            node.val = list.get(i);
+            
+            traverse.next = node;
+            node.prev = traverse;
+            
+            traverse = traverse.next;
+            
+            
+        }
+        
+        return res;
+    }
+    
+    public void dfs(Node traverse, Node next){
+        
+        if(traverse == null) return;
+        
+        Node curr = traverse;
+        
+        list.add(curr.val);
+        
+       if(traverse.child == null){
+           
+           dfs(traverse.next,next);
+       }
+        else{
+            
+            dfs(traverse.child,next);
+            dfs(traverse.next,next);
+        }       
+            
+        
+    }
+}
